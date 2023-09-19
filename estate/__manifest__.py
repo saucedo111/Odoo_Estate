f8e4e7940380cc51dd5f63f2bd5d37b229fc8029
saucedo111 <99573171+saucedo111@users.noreply.github.com>
2023-09-19 16:35:16 -0400

    Added fields and view to first test model, next step is to customize

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -24,9 +24,9 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-
+from dateutil.relativedelta import relativedelta
 from odoo import models, fields, api
 
 
@@ -6,12 +6,20 @@ class estate_Property(models.Model):
     _name = 'estate.estate.property'
     _description = 'Estate Properties'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(required=True)
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'),
+         ('sold', 'Sold'), ('canceled', 'Canceled')], default='new')
+    postcode = fields.Char()
+    description = fields.Text()
+    date_availability = fields.Date(copy=False, default=(fields.Date.today() + relativedelta(days=+90)))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
