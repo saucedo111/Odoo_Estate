a48cc3d5c7bf5cfe147af9fb21bfe5a80f4f57b9
saucedo111 <99573171+saucedo111@users.noreply.github.com>
2023-09-20 10:13:01 -0400

    push to test changing model name impacts

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,25 +1,26 @@
 from dateutil.relativedelta import relativedelta
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class estate_Property(models.Model):
-    _name = 'estate.estate.property'
+    _name = 'estate.property'
     _description = 'Estate Properties'
 
     name = fields.Char(required=True)
     active = fields.Boolean(default=True)
     state = fields.Selection(
-        [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'),
-         ('sold', 'Sold'), ('canceled', 'Canceled')], default='new')
+        [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'),
+         ('canceled', 'Canceled')], default='new')
     postcode = fields.Char()
     description = fields.Text()
     date_availability = fields.Date(copy=False, default=(fields.Date.today() + relativedelta(days=+90)))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
-    bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    bedrooms = fields.Integer(default=4)
+    living_area = fields.Integer(default=250)
     facades = fields.Integer()
     garage = fields.Boolean()
-    garden = fields.Boolean()
+    garden = fields.Boolean(default=True)
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+                                          default='North')
