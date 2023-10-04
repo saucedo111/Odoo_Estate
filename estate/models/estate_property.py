2076a9c73714e4d5360e19d7f45c7d7af6271dcd
saucedo111 <99573171+saucedo111@users.noreply.github.com>
2023-10-04 09:42:20 -0400

    form view finished, search options finished, move onto relations

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,21 +6,21 @@ class estate_Property(models.Model):
     _name = 'estate.property'
     _description = 'Estate Properties'
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, default='My New House')
     active = fields.Boolean(default=True)
     state = fields.Selection(
         [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'),
          ('canceled', 'Canceled')], default='new')
     postcode = fields.Char()
-    description = fields.Text()
     date_availability = fields.Date(copy=False, default=(fields.Date.today() + relativedelta(days=+90)))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=4)
     living_area = fields.Integer(default=250)
+    description = fields.Text(readonly=True, default='This is a beautiful house')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean(default=True)
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
-                                          default='North')
+                                          default='north')
